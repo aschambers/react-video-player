bfa7c09ef5cc4c251af1d894e5fe0cca5786eb1a
Alan Schambers <aschambers4291@gmail.com>
2020-05-18 20:38:38 -0700

    Add video player

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import VideoPlayer from './videoPlayer/VideoPlayer';
+import './App.css';
+
+const App = () => {
+  return (
+    <VideoPlayer />
+  );
+}
+
+export default App;
